7a83703dbd55bad9d89b83bb8dfec0e2ddeafc95
swanky-pants-janx <janxuse@protonmail.com>
2026-05-10 10:25:22 +0200

    Update supabase-client.js

--- js/lib/supabase-client.js
+++ js/lib/supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://qvgscualhjlbkmqkshmb.supabase.com';
+const SUPABASE_URL = 'https://qvgscualhjlbkmqkshmb.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_w9SloHM8F0EaF2UXXxxH9Q_34Nhs9AA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
